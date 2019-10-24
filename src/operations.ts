2bf4999e8b1f6be7ba953c223c4ea5fa33fcd7a3
Ilya <illiai@wix.com>
2019-10-24 19:05:15 +0300

    Add a sample spec for inter-column dnd

--- src/operations.ts
+++ src/operations.ts
@@ -1,25 +1,35 @@
-import { Board, Column } from "./Board/types";
+import { Board } from "./Board/types";
 import { DropResult } from "react-beautiful-dnd";
 
 export const handleDnd = (board: Board, result: DropResult): Board => {
-  if (!result.destination) return board;
+  const { destination, source } = result;
+  if (!destination) return board;
 
-  if (result.source.droppableId === result.destination.droppableId) {
-    const column = board.columns[result.destination.droppableId];
+  if (
+    destination.droppableId === source.droppableId &&
+    destination.index === source.index
+  )
+    return board;
+
+  if (result.source.droppableId === destination.droppableId) {
+    const column = board.columns[destination.droppableId];
     const newItems = [...column.items];
     const oldItem = column.items[result.source.index];
     newItems.splice(result.source.index, 1);
-    newItems.splice(result.destination.index, 0, oldItem);
+    newItems.splice(destination.index, 0, oldItem);
     return {
       ...board,
       columns: {
         ...board.columns,
-        [result.destination.droppableId]: {
+        [destination.droppableId]: {
           ...column,
           items: newItems
         }
       }
     };
+  } else {
+    console.log("different");
+    return board;
   }
 
   return board;
