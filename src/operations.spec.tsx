6d1a6196e5196c42e4066432c99e810571e98f90
Ilya <illiai@wix.com>
2019-10-24 19:05:15 +0300

    Add a sample spec for inter-column dnd

--- src/operations.spec.tsx
+++ src/operations.spec.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { Board } from "./Board/types";
 import { handleDnd } from "./operations";
 import { DropResult } from "react-beautiful-dnd";
 
-it("renders without crashing", () => {
+it("dragging within the column", () => {
   const event: DropResult = {
     draggableId: "CARD_1",
     type: "Card",
@@ -15,7 +14,7 @@ it("renders without crashing", () => {
 
   const sampleBoard: Board = {
     columns: {
-      "BOARD_1": {
+      BOARD_1: {
         items: [
           {
             id: "CARD_1",
@@ -31,10 +30,69 @@ it("renders without crashing", () => {
         name: "MyBoard"
       }
     },
-    columnOrders: ["1"]
+    columnOrders: ["BOARD_1"]
   };
 
   const result = handleDnd(sampleBoard, event);
 
-  expect(result.columns['BOARD_1'].items.map(i => i.id)).toEqual(["CARD_2", "CARD_1"]);
+  expect(result.columns["BOARD_1"].items.map(i => i.id)).toEqual([
+    "CARD_2",
+    "CARD_1"
+  ]);
+});
+
+it("dragging between two columns", () => {
+  const event: DropResult = {
+    draggableId: "CARD_1_1",
+    type: "Card",
+    source: { index: 0, droppableId: "COLUMN_1" },
+    reason: "DROP",
+    mode: "FLUID",
+    destination: { droppableId: "COLUMN_2", index: 1 }
+  };
+
+  const sampleBoard: Board = {
+    columns: {
+      COLUMN_1: {
+        items: [
+          {
+            id: "CARD_1_1",
+            text: "1"
+          },
+          {
+            id: "CARD_1_2",
+            text: "2"
+          }
+        ],
+        type: "PLAYLIST",
+        id: "COLUMN_1",
+        name: "MyBoard"
+      },
+      COLUMN_2: {
+        items: [
+          {
+            id: "CARD_2_1",
+            text: "1"
+          },
+          {
+            id: "CARD_2_2",
+            text: "2"
+          }
+        ],
+        type: "PLAYLIST",
+        id: "COLUMN_2",
+        name: "MyBoard"
+      }
+    },
+    columnOrders: ["COLUMN_1"]
+  };
+
+  const result = handleDnd(sampleBoard, event);
+
+  expect(result.columns["COLUMN_1"].items.map(i => i.id)).toEqual(["CARD_1"]);
+  expect(result.columns["COLUMN_2"].items.map(i => i.id)).toEqual([
+    "CARD_2_1",
+    "CARD_1_1",
+    "CARD_2_2"
+  ]);
 });
