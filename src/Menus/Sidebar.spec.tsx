5724607861fdee98cf82202a8206791293ad8e7c
Ilya <illiai@wix.com>
2019-10-31 20:08:38 +0200

    Fix TS in unit tests

--- src/Menus/Sidebar.spec.tsx
+++ src/Menus/Sidebar.spec.tsx
@@ -1,19 +1,20 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import {fireEvent, render} from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import Sidebar from "./Sidebar";
-import {DragDropContext} from "react-beautiful-dnd";
-import {useBoard} from "../state";
+import { DragDropContext } from "react-beautiful-dnd";
+import { useBoard } from "../state";
 
 interface Props {
   onSelectBoard?: (boardId: string) => void;
 }
 
-const SampleApp = ({onSelectBoard}: Props) => {
+const SampleApp = ({ onSelectBoard }: Props) => {
   const [state] = useBoard();
   return (
     <DragDropContext onDragEnd={() => "dummy"}>
       <Sidebar
+        onPlay={() => "dummy"}
         app={state}
         onSearchDone={() => "dummy"}
         onSelectBoard={onSelectBoard || (() => "dummy")}
@@ -23,19 +24,19 @@ const SampleApp = ({onSelectBoard}: Props) => {
 };
 
 it("a search should be shown by default", () => {
-  const {getByTestId} = render(<SampleApp/>);
+  const { getByTestId } = render(<SampleApp />);
   expect(getByTestId("search-input")).toBeInTheDocument();
 });
 
 it("clicking on a board button should show boards options", () => {
-  const {getByTestId, queryByTestId} = render(<SampleApp/>);
+  const { getByTestId, queryByTestId } = render(<SampleApp />);
   fireEvent.click(getByTestId("boards-button"));
   expect(queryByTestId("search-input")).not.toBeInTheDocument();
   expect(getByTestId("board-view")).toBeInTheDocument();
 });
 
 it("clicking on a board then on a search button should show search", () => {
-  const {getByTestId, queryByTestId} = render(<SampleApp/>);
+  const { getByTestId, queryByTestId } = render(<SampleApp />);
   fireEvent.click(getByTestId("boards-button"));
   fireEvent.click(getByTestId("search-button"));
   expect(queryByTestId("search-input")).toBeInTheDocument();
@@ -44,7 +45,7 @@ it("clicking on a board then on a search button should show search", () => {
 
 it("clicking on a board should call onSelectBoard with propert board id", () => {
   const onSelectBoard = jest.fn();
-  const {getByTestId} = render(<SampleApp onSelectBoard={onSelectBoard}/>);
+  const { getByTestId } = render(<SampleApp onSelectBoard={onSelectBoard} />);
   fireEvent.click(getByTestId("boards-button"));
   fireEvent.click(getByTestId("board-BOARD_1"));
   expect(onSelectBoard).toHaveBeenCalledWith("BOARD_1");
