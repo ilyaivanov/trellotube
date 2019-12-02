30497f66b42562c1c5a8ae3e0fcd5d5660dfb21d
Ilya <illiai@wix.com>
2019-12-02 18:40:01 +0200

    Skip all other items, like playlists for now

--- src/player/reducer.ts
+++ src/player/reducer.ts
@@ -2,7 +2,8 @@ import { ApplicationState } from "../infrastructure/types";
 import { Action, ACTIONS } from "../infrastructure/actions.types";
 
 export default (state: ApplicationState, action: Action): ApplicationState => {
-  if (action.type === ACTIONS.PLAY)
+  //skip all other items, like playlists for now
+  if (action.type === ACTIONS.PLAY && action.item.type === 'video')
     return {
       ...state,
       itemBeingPlayed: action.item
